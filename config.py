52c910163cd4169efac307489a5c9b06e6f61caa
py0327 <13936889275@163.com>
2025-06-08 09:21:59 +0800

    准备部署到 PythonAnywhere

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+SECRET_KEY = 'd62da6bdfd75bbbe54bf4e5fb527ca863e89d2254e0c002fe5dffdd923aed26d'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///user.db'  # 数据库文件名为 user.db
+
+
+# Cloudinary 配置
+CLOUDINARY_CLOUD_NAME = 'dugz0lsfp'
+CLOUDINARY_API_KEY = '749821665364848'
+CLOUDINARY_API_SECRET = '3uJODEjmoOe8xaoMdAuLnFu2AJQ'
+
+
